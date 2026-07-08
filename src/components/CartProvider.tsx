6cbb983c802d7712d7163f1aa239d2c5244830f1
buse191213-cmd <buse191213@gmail.com>
2026-07-08 15:30:58 +0200

    fix: add updateSizeBreakdown to useCart fallback (build error)

--- src/components/CartProvider.tsx
+++ src/components/CartProvider.tsx
@@ -154,6 +154,7 @@ export function useCart() {
       addItem: () => {},
       removeItem: () => {},
       updateQuantity: () => {},
+      updateSizeBreakdown: () => {},
       clearCart: () => {},
       isLoaded: false,
     };
